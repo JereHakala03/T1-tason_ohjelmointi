dc6a741e8898a6e0b09a3228fab19497f2f15698
JereHakala03 <89012002+JereHakala03@users.noreply.github.com>
2021-09-16 11:29:44 +0300

    Add files via upload

--- Kahvila/Kahvila/Form1.cs
+++ Kahvila/Kahvila/Form1.cs
@@ -10,11 +10,61 @@ using System.Windows.Forms;
 
 namespace Kahvila
 {
-    public partial class Form1 : Form
+    public partial class ruokalistaForm : Form
     {
-        public Form1()
+        public ruokalistaForm()
         {
             InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void meistaPL_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ruokalistaForm_Load(object sender, EventArgs e)
+        {
+            meistaLB.Text = "keudan oppilaskunnan kahvila palvelee luokassa 320 välituntisin \n";
+            meistaLB.Text += "9:00 - 14:30 joka päivä maanantaista torstaisiin ja perjantaisin \n";
+            meistaLB.Text += "9:00 - 12:30. \n\n keudan oppilaskunnan kahvilasta lämpimien \n";
+            meistaLB.Text += "juomien lisäksi virvokkeita ja lisäksi pientä purtavaa sekä makeista \n";
+            meistaLB.Text += "Tervetuloa tutustumaan!";
+            meistaLB.Font = new Font("Arial", 12);
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MeistäBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ruoatBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void juomatBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void herkutBT_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
