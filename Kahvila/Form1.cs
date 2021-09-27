9a63df4713db9b0206b2ae6a02d9805109915674
JereHakala03 <89012002+JereHakala03@users.noreply.github.com>
2021-09-27 12:44:26 +0300

    Add files via upload

---
+++ Kahvila/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kahvila
+{
+    public partial class ruokalistaForm : Form
+    {
+        public ruokalistaForm()
+        {
+            InitializeComponent();
+            meistaPL.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = false;
+            herkutPL.Visible = false;
+            koriPL.Visible = true;
+        }
+
+        private void meistaPL_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ruokalistaForm_Load(object sender, EventArgs e)
+        {
+            label.Text = "keudan oppilaskunnan kahvila palvelee luokassa 320 välituntisin \n";
+            label.Text += "9:00 - 14:30 joka päivä maanantaista torstaisiin ja perjantaisin \n";
+            label.Text += "9:00 - 12:30. \n\n keudan oppilaskunnan kahvilasta lämpimien \n";
+            label.Text += "juomien lisäksi virvokkeita ja lisäksi pientä purtavaa sekä makeista \n";
+            label.Text += "Tervetuloa tutustumaan!";
+            label.Font = new Font("Arial", 12);
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MeistäBT_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = true;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = false;
+            herkutPL.Visible = false;
+            koriPL.Visible = false;
+        }
+
+        private void ruoatBT_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = true;
+            juomatPL.Visible = false;
+            herkutPL.Visible = false;
+            koriPL.Visible = false;
+        }
+
+        private void juomatBT_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = true;
+            herkutPL.Visible = false;
+            koriPL.Visible = false;
+        }
+
+        private void herkutBT_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = false;
+            herkutPL.Visible = true;
+            koriPL.Visible = false;
+        }
+    }
+}
